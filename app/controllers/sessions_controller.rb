3e668761021384223eea194989e8c318961e8702
Reid Gillette <reid.gillette@gmail.com>
2011-09-05 16:49:30 -0500

    Added remember me persistence to sessions.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,16 @@
 class SessionsController < ApplicationController
   def new
+    redirect_to current_user if current_user
   end
 
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+      if params[:remember_me]
+        cookies.permanent[:auth_token] = user.auth_token
+      else
+        cookies[:auth_token] = user.auth_token
+      end
       redirect_to user, :notice => "Logged in!"
     else
       flash.now.alert = "Invalid email or password"
@@ -14,7 +19,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
+    cookies.delete(:auth_token)
     redirect_to root_url, :notice => "Logged out!"
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,11 @@ class User < ActiveRecord::Base
   validates :email, :presence => true,
                     :uniqueness => true,
                     :email_format => true
+  before_create { generate_token(:auth_token) }
+  
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
+  end
 end
