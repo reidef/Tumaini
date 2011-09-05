ea4508f807c3e58ff290a006dd024f08cc9030e2
Reid Gillette <reid.gillette@gmail.com>
2011-09-05 14:05:13 -0500

    Update user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  attr_accessible :email, :password, :password_confirmation
+  
+  has_secure_password
+  
+  validates_presence_of :password, :on => :create
+  validates :password_confirmation, :presence => true, :on => :create
+  validates :email, :presence => true,
+                    :uniqueness => true,
+                    :email_format => true
 end
