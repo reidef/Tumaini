6f2747bf88a7a5df245b0bbec1958e1f7f14e684
Reid Gillette <reid.gillette@gmail.com>
2011-09-15 22:52:51 -0500

    Rescue CanCan exception: redirect to users_path with exception message.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to users_path, :alert => exception.message
+  end
+  
   private
   def current_user
     @current_user ||= User.find_by_auth_token(cookies[:auth_token]) if cookies[:auth_token]
