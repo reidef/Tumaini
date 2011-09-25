a1321dee9d9cf349d724411f5b76d30c9c1ddcde
Reid Gillette <reid.gillette@gmail.com>
2011-09-25 00:48:44 -0500

    Added remote_sessions controller tests.

--- spec/controllers/remote_sessions_controller_spec.rb
+++ spec/controllers/remote_sessions_controller_spec.rb
@@ -1,11 +1,24 @@
 require 'spec_helper'
 
 describe RemoteSessionsController do
-
+  before(:each) do
+    @app = mock_model ClientApp, :callback_url => "http://example.com/"
+    @current_user = mock_model User, :auth_token => "secret", :client_apps => @app
+    controller.stub!(:current_user).and_return(@current_user)
+    request.cookies[:client_token] = "token"
+  end
+  
   describe "GET 'create'" do
-    it "should be successful" do
+    it "redirects to the client app if user is authorized to access it" do
+      @app.should_receive(:find_by_api_key).with("token").and_return(@app)
       get 'create'
-      response.should be_success
+      response.should redirect_to "http://example.com/secret"
+    end
+    
+    it "redirects to the user's page with message if user is not authorized" do
+      @app.should_receive(:find_by_api_key).with("token").and_return(nil)
+      get 'create'
+      response.should redirect_to @current_user
     end
   end
 
