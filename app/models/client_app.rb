711705e421cebc51a6bb9b46a76244eab593ebe7
Reid Gillette <reid.gillette@gmail.com>
2011-12-05 21:40:56 -0600

    Generate api key on create

--- app/models/client_app.rb
+++ app/models/client_app.rb
@@ -1,8 +1,13 @@
 class ClientApp < ActiveRecord::Base
+  include TokenGenerator
+  
+  attr_accessible :name
+  
   has_and_belongs_to_many :organizations
   has_and_belongs_to_many :users
   
   validates :name, :presence => true,
                    :uniqueness => true
-  validates :api_key, :presence => true
+  
+  before_create { generate_token(:api_key) }
 end
